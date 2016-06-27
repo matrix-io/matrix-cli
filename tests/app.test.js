d0baef9038b23c35f1c4a0be1fed7cdf0e7d9bb3
Sean Canton <eighteyes@gmail.com>
2016-06-27 11:14:09 -0700

    test stubs

---
+++ tests/app.test.js
@@ -0,0 +1,22 @@
+  describe('has application management functions', function () {
+    it('`matrix list apps`')
+    it('`matrix search`')
+    // it('`matrix search -s`')
+    it('`matrix install`')
+    it('`matrix install sensor`')
+    it('`matrix uninstall`')
+    // it('`matrix update`')
+    it('`matrix start`')
+    it('`matrix stop`')
+    it('`matrix set config`')
+    it('`matrix set config app`')
+    it('`matrix set config app key`')
+    it('`matrix set config app key=value`')
+  })
+
+  describe('has application development functions', function(){
+    it('`matrix create`')
+    it('`matrix deploy`')
+    it('`matrix trigger`')
+    it('`matrix log`')
+  })
---
+++ tests/device.test.js
@@ -0,0 +1,19 @@
+
+
+  describe('has device management functions', function(){
+    describe('supports a simulator', function(){
+      it('`matrix sim init`')
+      it('`matrix sim restore`')
+      it('`matrix sim start`')
+      it('`matrix sim stop`')
+      it('`matrix sim save`')
+      it('`matrix sim clear`')
+    })
+
+    it('`matrix use`')
+    // Matrix.deviceId;
+    it('`matrix list devices`')
+    it('`matrix reboot`')
+
+    // it('can list all groups')
+  })
