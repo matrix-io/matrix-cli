c8f3f14eb3da3515fbafac2ac4e41ff33ddf7196
Diego Ribero <diego.ribero@admobilize.com>
2016-07-21 18:56:45 -0500

    Async localization implemented, matrix use updated

--- bin/matrix-use.js
+++ bin/matrix-use.js
@@ -1,56 +1,58 @@
 require('./matrix-init');
 var localization = require('../lib/localization');
 var program = require('commander');
+var text;
 
-program
-  .parse(process.argv);
-var cmd = program.args;
+localization.init(__dirname + '/../config/locales', "en", function () {
+  text = localization.get;
+  program
+    .parse(process.argv);
+  var cmd = program.args;
 
-var debug = debugLog('use');
+  var debug = debugLog('use');
 
-if (showTheHelp){
-  showHelp();
-}
-var targetDevice = cmd[0];
-//TODO: exit if no targetDevice
-//TODO: store device list locally
-if (!_.isUndefined(targetDevice)) {
+  if (showTheHelp){
+    showHelp();
+  }
+  var targetDevice = cmd[0];
+  //TODO: exit if no targetDevice
+  //TODO: store device list locally
+  if (!_.isUndefined(targetDevice)) {
 
-  Matrix.api.device.register(targetDevice, function(err, state) {
-    if (state.status === "OK") {
-      var name = Matrix.helpers.lookupDeviceName(targetDevice);
+    Matrix.api.device.register(targetDevice, function(err, state) {
+      if (state.status === "OK") {
+        var name = Matrix.helpers.lookupDeviceName(targetDevice);
 
-      if (!_.isUndefined(name)) {
-        console.log('Now using device:'.grey, name);
+        if (!_.isUndefined(name)) {
+          console.log(text('matrix.use.using_device_by_name').grey, name);
+        } else {
+          console.log(text('matrix.use.using_device_by_id').grey, targetDevice);
+        }
+
+        // Save the device token
+        Matrix.config.device = {}
+        Matrix.config.device.identifier = targetDevice;
+        Matrix.config.device.token = state.results.device_token;
+        Matrix.helpers.saveConfig(process.exit);
+        
       } else {
-        console.log('Now using device id:'.grey, targetDevice);
+        debug('Matrix Use Error Object:', state);
+        if ( state.error === 'access_token not valid.' ) {
+          console.log(text('matrix.use.not_authorized').red, '\n', text('matrix.use.invalid_token'), ' ' , text('matrix.use.try').grey, 'matrix login')
+        } else {
+          console.error('Error', state.status_code.red, state.error);
+        }
       }
 
-      // Save the device token
-      Matrix.config.device = {}
-      Matrix.config.device.identifier = targetDevice;
-      Matrix.config.device.token = state.results.device_token;
-      Matrix.helpers.saveConfig(process.exit);
-      
-    } else {
-      debug('Matrix Use Error Object:', state);
-      if ( state.error === 'access_token not valid.' ) {
-        console.log('Not Authorized'.red, '\nYour token is invalid. Try'.grey, 'matrix login')
-      } else {
-        console.error('Error', state.status_code.red, state.error);
-      }
-    }
+    });
 
-  });
+  } else {
+    showHelp();
+  }
 
-} else {
-  showHelp();
-}
-
-function showHelp() {
-  console.log('\n> matrix use ¬ \n');
-  //console.log('\t                 matrix use <deviceid> -', 'set active device to device id'.grey)
-  console.log('\t                 matrix use <deviceid> -', localization.get('matrix.use.help').grey)
-  
-  console.log('\n')
-}
+  function showHelp() {
+    console.log('\n> matrix use ¬ \n');
+    console.log('\t                 matrix use <deviceid> -', text('matrix.use.help').grey)    
+    console.log('\n')
+  }
+});
