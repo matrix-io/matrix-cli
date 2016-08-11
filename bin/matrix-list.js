4ae452c1f3a4103539c23680fd6b39252f697985
Sean Canton <eighteyes@gmail.com>
2016-08-11 15:04:24 -0700

    merge

--- bin/matrix-list.js
+++ bin/matrix-list.js
@@ -6,63 +6,42 @@ var debug = debugLog('sdk');
 
 Matrix.localization.init(Matrix.localesFolder, Matrix.config.locale, function () {
 
-  program
-    .parse(process.argv);
-  var pkgs = program.args;
+program
+.parse(process.argv);
+var pkgs = program.args;
 
-  if (!pkgs.length || showTheHelp) {
-    displayHelp();
-  }
+if (!pkgs.length || showTheHelp) {
+  displayHelp();
+}
 
-  var target = pkgs[0];
+var target = pkgs[0];
+
+var firebase = require('matrix-firebase')
+
+firebase.init(
+  Matrix.config.user.id,
+  Matrix.config.device.identifier,
+  Matrix.config.user.token,
+  function (err){
+    if (err) return console.error(err);
 
 
-  if (target.match(/all/)) {
-    Matrix.api.device.getAppList(Matrix.config.device.identifier, function (err, resp) {
-      if (err) return console.error(t('matrix.list.app_list_error') + ':', err);
-      if (_.isEmpty(resp)) return console.error(t('matrix.list.no_results'));
-      debug('Device List>', resp);
-      console.log(Matrix.helpers.displayDeviceApps(resp));
-    });
+    if (target.match(/all/)){
+      Matrix.api.device.getAppList(Matrix.config.device.identifier, function(err, resp){
+        if (err) return console.error('App List Error:', err);
+        if (_.isEmpty(resp)) return console.error('No Results Found');
+        debug('Device List>', resp);
+        console.log( Matrix.helpers.displayDeviceApps(resp) );
+      });
 
+    } else if (target.match(/app/)) {
 
-  } else if (target.match(/app/)) {
-
-    Matrix.api.app.list(function (apps) {
-      console.log(Matrix.helpers.displayApps(apps));
-      process.exit();
-    });
-
-  } else if (target.match(/device/)) {
-
-    var group = pkgs[1];
-    /** do nothing if not device **/
-    if (group !== undefined) {
-      // Matrix.api.user.setToken(Matrix.config.user.token);
-      var options = {
-        group: group
-      };
-      Matrix.api.device.list(options, function (body) {
-        //print device
-        console.log(Matrix.helpers.displayDevices(body));
+      firebase.app.list( function(err, apps){
+        console.log(Matrix.helpers.displayApps(apps));
         process.exit();
       });
-    } else {
-      Matrix.api.device.list({}, function (body) {
-        //print device
-        console.log(Matrix.helpers.displayDevices(body));
-        // save device map to config
-        Matrix.config.deviceMap = _.map(JSON.parse(body).results, function (d) {
-          return { name: d.name, id: d.deviceId }
-        });
-        Matrix.helpers.saveConfig(function () {
-          process.exit();
-        });
 
-      });
-    }
-
-  } else if (target.match(/group/)) {
+     } else if (target.match(/group/)) {
 
     /** do nothing if not device **/
     Matrix.api.group.list(function (body) {
@@ -73,6 +52,7 @@ Matrix.localization.init(Matrix.localesFolder, Matrix.config.locale, function ()
   } else {
     displayHelp();
   }
+});
 
   function displayHelp() {
     console.log('\n> matrix list ¬\n');
