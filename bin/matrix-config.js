e360b1f89ac1692bd63956b6aebb9f6d5877d385
Sean Canton <eighteyes@gmail.com>
2016-08-11 15:24:30 -0700

    matrix config

--- bin/matrix-config.js
+++ bin/matrix-config.js
@@ -8,11 +8,11 @@ var debug = debugLog('config');
 Matrix.localization.init(Matrix.localesFolder, Matrix.config.locale, function () {
 
   program
-    .parse(process.argv);
+  .parse(process.argv);
 
   var pkgs = program.args;
 
-
+  // log(pkgs, pkgs.length)
 
   if (_.isUndefined(Matrix, 'config.user.token')) {
     return console.error(t('matrix.please_login') + ' `matrix login`'.grey);
@@ -32,131 +32,123 @@ Matrix.localization.init(Matrix.localesFolder, Matrix.config.locale, function ()
 
       var options = {};
 
-      console.log('Config for device', Matrix.config.device.identifier);
 
-      firebase.device.getConfig(handleResponse);
-
-      // matrix config base
-    } else if ( pkgs.length === 1 ){
-
-      var target = pkgs[0]
-      firebase.app.get( target, handleResponse);
-      // matrix config base services.vehicle.engine
+      if (pkgs.indexOf('--watch') > -1 || pkgs.indexOf('-w') > -1) {
+        options.watch = true;
+      }
 
       if (pkgs.indexOf('--help') > -1 || pkgs.indexOf('-h') > -1) {
         showHelp();
-      } else
-        // show matrix config
-        if (pkgs.length === 0) {
-          // get form
+      } else if (pkgs.length === 0) {
+        // get form
 
-          console.log(t('matrix.config.device_config') + ' ', Matrix.config.device.identifier);
+        console.log(t('matrix.config.device_config') + ' ', Matrix.config.device.identifier);
 
-          firebase.device.get(handleResponse);
+        firebase.device.getConfig(handleResponse);
 
-          // matrix config base
-        } else if (pkgs.length === 1) {
+        // matrix config base
+      } else if (pkgs.length === 1) {
 
-          var target = pkgs[0]
-          firebase.app.get(target, handleResponse);
-          // matrix config base services.vehicle.engine
+        var target = pkgs[0]
+        firebase.app.get(target, handleResponse);
+        // matrix config base services.vehicle.engine
 
-          if (options.watch) {
-            firebase.app.onChange(target, handleResponse)
-          }
-
-        } else if (pkgs.length === 2) {
-          // get deep
-
-          var key = pkgs[1];
-          var target = pkgs[0] + '/' + key.replace(/\./g, '/');
-          debug('Firebase: '.blue, target)
-          firebase.app.get(target, handleResponse)
-
-          if (options.watch) {
-            firebase.app.onChange(target, handleResponse)
-          }
-          // matrix config base keywords=test,keyword
-        } else if (pkgs.length >= 3) {
-          var key = pkgs[1];
-          var value = pkgs[2];
-
-          //typing
-          if (pkgs.length > 4) {
-            //string
-            value = _.drop(pkgs, 2).join(' ');
-          } else {
-            // is array?
-            if (value.indexOf(',') > -1) {
-              value = value.split(',');
-            }
-          }
-
-          var target = pkgs[0] + '/' + key.replace(/\./g, '/');
-
-          debug(target, value);
-
-          firebase.app.set(target, value);
-
-        } else {
-          showHelp();
+        if (options.watch) {
+          firebase.app.onChange(target, handleResponse)
         }
+
+      } else if (pkgs.length === 2) {
+        // get deep
+
+        var key = pkgs[1];
+        var target = pkgs[0] + '/' + key.replace(/\./g, '/');
+        debug('Firebase: '.blue, target)
+        firebase.app.get(target, handleResponse)
+
+        if (options.watch) {
+          firebase.app.onChange(target, handleResponse)
+        }
+        // matrix config base keywords=test,keyword
+      } else if (pkgs.length >= 3) {
+        var key = pkgs[1];
+        var value = pkgs[2];
+
+        //typing
+        if (pkgs.length > 4) {
+          //string
+          value = _.drop(pkgs, 2).join(' ');
+        } else {
+          // is array?
+          if (value.indexOf(',') > -1) {
+            value = value.split(',');
+          }
+        }
+
+        var target = pkgs[0] + '/' + key.replace(/\./g, '/');
+
+        debug(target, value);
+
+        firebase.app.set(target, value);
+
+      } else {
+        showHelp();
+      }
     });
 
 
-function handleResponse(err, app){
-  if (err) return console.error(err);
-  if (_.isNull(app)){ console.log(t())}
-  console.log(require('util').inspect(app, {depth: 3, colors:true}));
-  process.exit();
-}
+    function handleResponse(err, app){
+      if (err) return console.error(t('matrix.helpers.config_error'), err);
+      if (_.isNull(app)){ console.error( t('matrix.helpers.config_error'), app) }
+      console.log(require('util').inspect(app, {depth: 3, colors:true}));
+      process.exit();
+    }
 
 
-  function showHelp() {
+    function showHelp() {
 
-    console.log('\n> matrix config ¬\n');
-    console.log('\t         matrix config -', t('matrix.config.help').grey)
-    console.log('\tmatrix config <appName> -', t('matrix.config.help_app').grey)
-    console.log('\tmatrix config <appName> <key> -', t('matrix.config.help_app_key').grey)
-    console.log('\tmatrix config <appName> <key> <value> -', t('matrix.config.help_app_key_value').grey)
-    console.log('\n')
-    process.exit(1);
-  }
-  //
-  //
-  //
-  // var appName = pkgs[1];
-  // var configStr = pkgs[2];
-  // var key, val;
-  //
-  //
-  // if (_.isUndefined(appName)) {
-  //   console.warn('App Name Required: `matrix set config <appName> [key=value]` ')
-  //   process.exit(0);
-  // }
-  //
-  // if (!_.isUndefined(configStr) && configStr.match(/=/)) {
-  //   // key=value
-  //   key = configStr.split('=')[0].trim();
-  //   val = configStr.split('=')[1].trim();
-  // } else {
-  //   console.warn('Must supply key and value: `matrix set config <appName> [key=value]`');
-  //   process.exit(0);
-  // }
-  //
-  // // validate keys
-  // key = _.snakeCase(key.toLowerCase());
-  //
-  // var options = {
-  //   deviceId: Matrix.config.device.id,
-  //   name: appName,
-  //   key: key,
-  //   value: val
-  // }
-  //
-  // Matrix.api.app.configure(options, function(err, resp) {
-  //   if (err) console.error(err);
-  //   console.log('[' + options.deviceId + '](' + options.name + ')', options.key, '=', options.value);
-  //   setTimeout(process.exit, 1000);
-  // });
-});
+      console.log('\n> matrix config ¬\n');
+      console.log('\t         matrix config -', t('matrix.config.help').grey)
+      console.log('\tmatrix config <appName> -', t('matrix.config.help_app').grey)
+      console.log('\tmatrix config <appName> <key> -', t('matrix.config.help_app_key').grey)
+      console.log('\tmatrix config <appName> <key> <value> -', t('matrix.config.help_app_key_value').grey)
+      console.log('\n')
+      process.exit(1);
+    }
+    //
+    //
+    //
+    // var appName = pkgs[1];
+    // var configStr = pkgs[2];
+    // var key, val;
+    //
+    //
+    // if (_.isUndefined(appName)) {
+    //   console.warn('App Name Required: `matrix set config <appName> [key=value]` ')
+    //   process.exit(0);
+    // }
+    //
+    // if (!_.isUndefined(configStr) && configStr.match(/=/)) {
+    //   // key=value
+    //   key = configStr.split('=')[0].trim();
+    //   val = configStr.split('=')[1].trim();
+    // } else {
+    //   console.warn('Must supply key and value: `matrix set config <appName> [key=value]`');
+    //   process.exit(0);
+    // }
+    //
+    // // validate keys
+    // key = _.snakeCase(key.toLowerCase());
+    //
+    // var options = {
+    //   deviceId: Matrix.config.device.id,
+    //   name: appName,
+    //   key: key,
+    //   value: val
+    // }
+    //
+    // Matrix.api.app.configure(options, function(err, resp) {
+    //   if (err) console.error(err);
+    //   console.log('[' + options.deviceId + '](' + options.name + ')', options.key, '=', options.value);
+    //   setTimeout(process.exit, 1000);
+    // });
+  });
