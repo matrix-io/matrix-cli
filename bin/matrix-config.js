873382ba98213e89f75c5d4cbe17a5f02e23811b
Diego Ribero <diego.ribero@admobilize.com>
2016-08-30 15:22:55 -0500

    Typo fix

--- bin/matrix-config.js
+++ bin/matrix-config.js
@@ -15,7 +15,7 @@ Matrix.localization.init(Matrix.localesFolder, Matrix.config.locale, function ()
   // log(pkgs, pkgs.length)
 
   if (_.isUndefined(Matrix, 'config.user.token')) {
-    return console.error(t('matrix.please_login') + ' `matrix register`'.grey);
+    return console.error(t('matrix.please_login') + ' `matrix login`'.grey);
   }
 
   if (_.isUndefined(Matrix.config.device.identifier)) {
