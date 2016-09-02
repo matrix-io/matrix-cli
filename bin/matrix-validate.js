1ceb0992b4317408f849baf651f6020b015d76ca
Diego Ribero <diego.ribero@admobilize.com>
2016-09-02 09:28:05 -0500

    Device validation update

--- bin/matrix-validate.js
+++ bin/matrix-validate.js
@@ -8,7 +8,7 @@ function user() {
 
 //Check if a device is selected
 function device() {
-  if (_.isEmpty(Matrix.config.device.token)) {
+  if (_.isEmpty(Matrix.config.device) || _.isUndefined(Matrix.config.device.token)) {
     console.error(t('matrix.validate.no_device') + '\n', '\nmatrix list devices'.grey,' - > '.yellow + t('matrix.validate.select_device_id').yellow, '\nmatrix use\n'.grey)
     process.exit();
   } 
