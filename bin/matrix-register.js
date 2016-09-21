d4d4a2449962bb5b8d80546fb5b41bf9d1696a57
Sean Canton <eighteyes@gmail.com>
2016-09-21 12:42:09 -0700

    Device Registration Stat

--- bin/matrix-register.js
+++ bin/matrix-register.js
@@ -3,89 +3,133 @@
 require('./matrix-init');
 var prompt = require('prompt');
 var debug = debugLog('register');
-var request = require('request');
 
 Matrix.localization.init(Matrix.localesFolder, Matrix.config.locale, function () {
-  var schema = {
-    properties: {
-      username: {
-        required: true,
-        pattern: /\S+@\S+\.\S+/,
-        message: 'Username must be a valid email',
-        description: 'Username: '
-      },
-      password: {
-        required: true,
-        pattern: /^(?=.*?[a-zA-Z])(?=.*?[0-9]).{6,}/,
-        message: 'Password must be at least 6 characters long and should contain a lower case letter and a number',
-        hidden: true,
-        description: 'Password: '
-      },
-      confirmPassword: {
-        hidden: true,
-        description: 'Confirm Password: '
-      }
-    }
-  };
+  if (Matrix.pkgs.length > 0) {
+    var command = Matrix.pkgs[0];
+    if (command !== 'device') {
+      console.warn('matrix register', command, 'is not a valid command');
+      process.exit(1);
+    } else {
+      // do device Registration
+      var deviceSchema = {
+        properties: {
+          name: {
+            required: true,
+            description: 'device name'
+          },
+          // for when this is ready
+          // serial: {
+          //   required: true
+          //   description: 'serial number'
+          // }
+        }
+      };
+      prompt.delimiter = '';
+      prompt.message = 'Device Registration -- ';
+      prompt.start();
+      prompt.get(deviceSchema, function (err, result) {
 
-  prompt.delimiter = '';
-  prompt.message = 'Registration -- ';
-  prompt.start();
-  prompt.get(schema, function (err, result) {
-    if (err) {
-      if (err.toString().indexOf('canceled') > 0) {
-        console.log('');
-        process.exit();
-      } else { 
-        console.log("Error: ", err);
-        process.exit();
-      }
-    }
-    if (result.password != result.confirmPassword) {
-      return console.error("Passwords didn't match");
-    }
+        // Matrix.helper.registerDevice()....
 
-    /** set the creds **/
-    Matrix.config.user = {
-      username: result.username,
-      password: result.password
+        // all of the below is untested - next line = matrix use
+        Matrix.config.device.identifier = result.deviceId;
+
+
+        Matrix.helpers.saveConfig(function () {
+          process.exit();
+        });
+
+        // this part will be automated in the future. idk how.
+        console.log('Set the following environment variables on your rpi.')
+        console.log('MATRIX_DEVICE_ID=', result.deviceId);
+
+        // return the secret
+        console.log('MATRIX_DEVICE_SECRET=', result.secret)
+      })
+    }
+  } else {
+
+    var schema = {
+      properties: {
+        username: {
+          required: true,
+          pattern: /\S+@\S+\.\S+/,
+          message: 'Username must be a valid email',
+          description: 'Username: '
+        },
+        password: {
+          required: true,
+          pattern: /^(?=.*?[a-zA-Z])(?=.*?[0-9]).{6,}/,
+          message: 'Password must be at least 6 characters long and should contain a lower case letter and a number',
+          hidden: true,
+          description: 'Password: '
+        },
+        confirmPassword: {
+          hidden: true,
+          description: 'Confirm Password: '
+        }
+      }
     };
-    
-    Matrix.config.user.jwt_token = true;
 
-    Matrix.config.client = {};
-    debug('Client', Matrix.options);
-    
-    /*request.post(params, function(error, response, body) {
-    };*/
+    prompt.delimiter = '';
+    prompt.message = 'Registration -- ';
+    prompt.start();
+    prompt.get(schema, function (err, result) {
+      if (err) {
+        if (err.toString().indexOf('canceled') > 0) {
+          console.log('');
+          process.exit();
+        } else {
+          console.log('Error: ', err);
+          process.exit();
+        }
+      }
+      if (result.password !== result.confirmPassword) {
+        return console.error('Passwords didn\'t match');
+      }
+
+      /** set the creds **/
+      Matrix.config.user = {
+        username: result.username,
+        password: result.password
+      };
+
+      Matrix.config.user.jwt_token = true;
+
+      Matrix.config.client = {};
+      debug('Client', Matrix.options);
+
+      /*request.post(params, function(error, response, body) {
+      };*/
 
 
-    Matrix.api.register.user(result.username, result.password, Matrix.options.clientId, function (err, out) {
-      /*500 server died
-      400 bad request
-        user exists
-        missing parameter
-      401*/ 
-      if (err){
-        if(err.hasOwnProperty('status_code')){
-            if(err.status_code == 500){
+      Matrix.api.register.user(result.username, result.password, Matrix.options.clientId, function (err, out) {
+        /*500 server died
+        400 bad request
+          user exists
+          missing parameter
+        401*/
+        if (err) {
+          if (err.hasOwnProperty('status_code')) {
+            if (err.status_code === 500) {
               console.log('Server unavailable, please try again later');
-            } else if (err.status_code == 400){
+            } else if (err.status_code === 400) {
               console.log('Unable to create user ' + result.username + ', user already exists');
             } else {
               console.log('Unknown error (' + err.status_code + '): ', err);
             }
+          } else {
+            console.error('Unknown error: ', err);
+          }
         } else {
-          console.error("Unknown error: ", err);
+          debug('User', Matrix.config.user, out);
+          console.log('User ' + result.username + ' successfully created');
         }
-      } else {
-        debug('User', Matrix.config.user, out);
-        console.log('User ' + result.username + ' successfully created');
-      }
-      process.exit();
+        process.exit();
+      });
+
+
     });
-
-
-    //process.exit();
-  });
+  }
 });
