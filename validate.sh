0bbda1384669d5fd6b449b1a04d14ff3ba6f14a2
BRAYANP <b-d-p-t@hotmail.com>
2017-03-21 11:12:11 -0500

    Resolved issue validate for others OS ..

--- validate.sh
+++ validate.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+if [ -f /etc/os-release ]
+then
 if  grep -qR "Raspbian"  "/etc/os-release"
 then
  echo "THIS IS A RASPBERRY DON'T INSTALL MATRIX CLI IN THIS MACHINE"
@@ -11,6 +13,9 @@ then
         * ) echo "Aborting installation"; exit 1;
     esac
 done
+else
+ exit 0
+fi
 else
  exit 0
 fi
