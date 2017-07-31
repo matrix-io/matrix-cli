c9e78211b9dd5c3780ee3cbecd7e64e3d80d65f1
Lucas <lucas.ecomp2012@gmail.com>
2017-07-31 12:32:27 -0300

    Adding publish/unpublish help at en.yml and es.yml

--- bin/matrix-unpublish.js
+++ bin/matrix-unpublish.js
@@ -27,70 +27,69 @@ async.series([
   if (!Matrix.pkgs.length || showTheHelp) return displayHelp();
   var target = Matrix.pkgs[0];
 
-  console.log('____ | ' + t('matrix.unpublish.unpublishing'));
+  console.log('____ | ' + 'Searching for app');
   Matrix.loader.start();
 
   //get app id
-  Matrix.firebase.app.search(target, function(err, data) {
+  Matrix.firebase.app.search(target, function(data) {
+
     Matrix.loader.stop();
-    if (err) {
-      console.log('Error: '.red, err.message);
-      process.exit();
-    }
-    if (!data) {
-      console.log('\nApp not found... ');
+    if (_.isEmpty(data)){
+      console.log('\n App not found!');
       process.exit(1);
-    } else if (data.acl.ownerId === Matrix.config.user.id){
-      console.log('\nApp found... ');
-
-      var progress;
-      Matrix.loader.start();
-
-      Matrix.helpers.trackEvent('app-unpublish', { aid: target, did: Matrix.config.device.identifier });
-
-      Matrix.firebase.app.unpublish(Matrix.config.user.token, Matrix.config.user.id, data.id, {
-        error: function (err) {
-          Matrix.loader.stop();
-          if (err && err.hasOwnProperty('details') && err.details.hasOwnProperty('error')) {
-            console.error('\nUnpublish Error'.red, err.details.error);
-          } else {
-            console.error('\nUnpublish Error'.red, err);
-          }
-          process.exit(1);
-        },
-        finished: function() {
-          Matrix.loader.stop();
-          console.log('Unpublishing app...'.green);
-          process.exit(0);
-        },
-        start: _.once(function() {
-          Matrix.loader.stop();
-          console.log('Unpublish request created');
-          Matrix.loader.start();
-        }),
-        progress: function(msg) {
-          if (_.isUndefined(msg)) msg = '';
-          else msg = ' ' + msg + ' ';
-          if (!progress) {
-            progress = true;
-            Matrix.loader.stop();
-            process.stdout.write('Unpublish progress:' + msg);
-          } else {
-            process.stdout.write('.' + msg);
-          }
-        }
-      });
     } else {
-      console.log('\n This app does not belongs to this user. Please try to unpublish logged at the right user!'.red);
-      process.exit(1);
+      console.log(data);
+      if (data.acl.ownerId !== Matrix.config.user.id) {
+        console.log('\n This app does not belongs to this user!'.red);
+        process.exit(1);
+      } else {
+        console.log('\nApp found... ');
+
+        var progress;
+        Matrix.loader.start();
+
+        Matrix.helpers.trackEvent('app-unpublish', { aid: target, did: Matrix.config.device.identifier });
+
+        console.log('____ | ' + t('matrix.unpublish.unpublishing'));
+        Matrix.firebase.app.unpublish(Matrix.config.user.token, Matrix.config.user.id, data.id, {
+          error: function (err) {
+            Matrix.loader.stop();
+            if (err && err.hasOwnProperty('details') && err.details.hasOwnProperty('error')) {
+              console.error('\n Unpublish Error'.red, err.details.error);
+            } else {
+              console.error('\n Unpublish Error'.red, err);
+            }
+            process.exit(1);
+          },
+          finished: function() {
+            Matrix.loader.stop();
+            console.log('Unpublishing app...'.green);
+            process.exit(0);
+          },
+          start: _.once(function() {
+            Matrix.loader.stop();
+            console.log('Unpublish request created');
+            Matrix.loader.start();
+          }),
+          progress: function(msg) {
+            if (_.isUndefined(msg)) msg = '';
+            else msg = ' ' + msg + ' ';
+            if (!progress) {
+              progress = true;
+              Matrix.loader.stop();
+              process.stdout.write('Unpublish progress:' + msg);
+            } else {
+              process.stdout.write('.' + msg);
+            }
+          }
+        });
+      }
     }
-
-
   });
 
   function displayHelp() {
     console.log('\n> matrix unpublish ¬\n');
-    console.log('\t    matrix unpublish <app> -', t('matrix.unpublish.help_app', { app: '<app>' }).grey);
+    console.log('\t    matrix unpublish <app> -', t('matrix.unpublish.help', { app: '<app>' }).grey);
     console.log('\n');
     process.exit(1);
   }
