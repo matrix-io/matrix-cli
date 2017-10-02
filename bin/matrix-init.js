9a8608ca2d4a8a1177ba29d24b8efbaafebd548a
Sean Canton <eighteyes@gmail.com>
2017-10-02 16:23:39 -0700

    Switch to production environment

--- bin/matrix-init.js
+++ bin/matrix-init.js
@@ -39,8 +39,8 @@ if (_.has(Matrix.config, 'trackOk')) {
 var options = {
   clientId: 'AdMobilizeAPIDev',
   clientSecret: 'AdMobilizeAPIDevSecret',
-  apiUrl: process.env['MATRIX_API_SERVER'] || 'https://rc-api.admobilize.com',
-  mxssUrl: process.env['MATRIX_STREAMING_SERVER'] || 'https://rc-mxss.admobilize.com',
+  apiUrl: process.env['MATRIX_API_SERVER'] || 'https://api.admobilize.com',
+  mxssUrl: process.env['MATRIX_STREAMING_SERVER'] || 'https://mxss.admobilize.com',
   appsBucket: process.env['MATRIX_APPS_BUCKET'] || 'admobilize-matrix-apps'
 };
 
@@ -51,7 +51,7 @@ Matrix.endIt = function (code, seconds) {
   if (seconds != 0) {
     setTimeout(function () {
       process.nextTick(function () { process.exit(code); });
-    }, seconds * 1000) 
+    }, seconds * 1000)
   } else {
     process.nextTick(function () { process.exit(code); });
   }
@@ -62,7 +62,7 @@ function init(finished) {
 
   // neeed the async to ensure config file is saved before continuing
   async.series([
-    function(cb) {
+    function (cb) {
       if (_.has(Matrix.config.environment, 'name')) {
         debug('Env: ', Matrix.config.environment.name);
         options.apiUrl = Matrix.config.environment.api;
@@ -71,11 +71,11 @@ function init(finished) {
       }
       cb();
     },
-    function(cb) {
+    function (cb) {
       if (!_.has(Matrix.config.environment, 'name')) {
         debug('No env set, using rc default');
         Matrix.config.environment = {
-          name: process.env.NODE_ENV || 'rc',
+          name: process.env.NODE_ENV || 'production',
           api: options.apiUrl,
           mxss: options.mxssUrl,
           appsBucket: options.appsBucket
@@ -109,44 +109,44 @@ function init(finished) {
 
     Matrix.firebase = require('matrix-firebase');
     Matrix.firebaseInit = function initFirebase(cb) {
-        var currentDevice = (!_.isEmpty(Matrix.config.device) && !_.isEmpty(Matrix.config.device.identifier)) ? Matrix.config.device.identifier : '';
-        debug('Firebase Init', Matrix.config.user.id, currentDevice);
-        Matrix.firebase.init(
-          Matrix.config.user.id,
-          currentDevice,
-          Matrix.config.user.token,
-          Matrix.config.environment.name,
-          function(err) {
-            if (err) { debug('firebase error', err) }
-            var errorCode = Matrix.validate.firebaseError(err);
-            if (errorCode !== 0) {
-              if (errorCode === 1) {
-                //TODO try to refresh token before failing
-                Matrix.loader.stop();
-                console.log('Invalid user, log in again'.yellow);
-                Matrix.helpers.logout(function() {
-                  process.exit();
-                });
+      var currentDevice = (!_.isEmpty(Matrix.config.device) && !_.isEmpty(Matrix.config.device.identifier)) ? Matrix.config.device.identifier : '';
+      debug('Firebase Init', Matrix.config.user.id, currentDevice);
+      Matrix.firebase.init(
+        Matrix.config.user.id,
+        currentDevice,
+        Matrix.config.user.token,
+        Matrix.config.environment.name,
+        function (err) {
+          if (err) { debug('firebase error', err) }
+          var errorCode = Matrix.validate.firebaseError(err);
+          if (errorCode !== 0) {
+            if (errorCode === 1) {
+              //TODO try to refresh token before failing
+              Matrix.loader.stop();
+              console.log('Invalid user, log in again'.yellow);
+              Matrix.helpers.logout(function () {
+                process.exit();
+              });
 
-              } else if (errorCode == 4) {
-                console.log('Network timeout, please check your connection and try again'.yellow);
-              } else {
-                console.error('Error initializing Firebase: '.yellow, err.message.red);
+            } else if (errorCode == 4) {
+              console.log('Network timeout, please check your connection and try again'.yellow);
+            } else {
+              console.error('Error initializing Firebase: '.yellow, err.message.red);
 
-                // specific info on how to resolve
-                if (err.code === 'auth/custom-token-mismatch') {
-                  console.error('Server environments may be incongruent.',
-                    'Rerun `%s` and login again to reset your configuration', 'matrix set env <dev/production>'.yellow
-                  );
-                }
+              // specific info on how to resolve
+              if (err.code === 'auth/custom-token-mismatch') {
+                console.error('Server environments may be incongruent.',
+                  'Rerun `%s` and login again to reset your configuration', 'matrix set env <dev/production>'.yellow
+                );
               }
-              process.exit();
             }
-            return cb();
-          });
-        //## firebase.init
-      }
-      //# firebaseInit
+            process.exit();
+          }
+          return cb();
+        });
+      //## firebase.init
+    }
+    //# firebaseInit
 
     // all done with setup
     finished();
